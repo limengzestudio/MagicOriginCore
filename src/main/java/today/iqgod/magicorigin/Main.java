69590f17ed2dacc5bd30fbeb02777ff2f7a10131
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-03-11 18:38:20 +0800

    PAPI

--- src/main/java/today/iqgod/magicorigin/Main.java
+++ src/main/java/today/iqgod/magicorigin/Main.java
@@ -1,13 +1,26 @@
 package today.iqgod.magicorigin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    public static Plugin instance;
+
+    public static boolean PAPI_enabled;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        try
+        {
+            PAPI_enabled = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
+            instance = this;
+        } catch (Exception ex) {
+            System.out.println("Unable to load MoCore");
+            ex.printStackTrace();
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
