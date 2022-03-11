edba0177e26c1d2d2f1421c2ad9f0c608e31c312
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-03-11 18:18:44 +0800

    initial commit

---
+++ src/main/java/today/iqgod/magicorigin/features/join/JoinMessage.java
@@ -0,0 +1,13 @@
+package today.iqgod.magicorigin.features.join;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinMessage implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e)
+    {
+
+    }
+}
