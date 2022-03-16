bdfe584f6777fa5a7d043196e7d1d67571e4988b
limengzestudio <91863577+limengzestudio@users.noreply.github.com>
2022-03-16 11:56:00 +0800

    增加 AntiBookBan 功能

---
+++ src/main/java/today/iqgod/magicorigin/AntiBookBan.java
@@ -0,0 +1,32 @@
+// 原作者 https://github.com/oldfagorg/AntiBookBan
+
+package today.iqgod;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEditBookEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+public class Main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEdit(PlayerEditBookEvent event) {
+        Iterator var2 = event.getNewBookMeta().getPages().iterator();
+        while(var2.hasNext()) {
+            String page = (String)var2.next();
+            if (!Charset.forName("ISO-8859-1").newEncoder().canEncode((page))) { 
+                event.setCancelled(true);
+            }
+        }
+
+    }
+
+}
